d97acc31a25c4412ee8544431c43186b5e7addf2
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-10-08 16:57:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+
+let select = document.getElementById("colorSelect");
+let button = document.getElementsByTagName("input")[0];
+
+button.addEventListener("click", () => {
+let selectedOption = select.options[select.selectedIndex];
+selectedOption.remove();
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
